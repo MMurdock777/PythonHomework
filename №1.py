bd3d9e1a65b65f2897a1271639b9950604aa1f2b
MMurdock777 <67927164+MMurdock777@users.noreply.github.com>
2022-11-12 21:28:05 +0300

    Python Homework 2

---
+++ №1.py
@@ -0,0 +1,8 @@
+team_name = input("Введите название команды:")
+print(f"{team_name} - чемпион!")
+print("-" * len(team_name))
+print(f"{team_name.lower()} - чемпион!")
+print("Длина названия: ", len(team_name))
+p_in_team = "п" in team_name
+print("Буква п в названии: ", "п" in team_name.lower())
+print("Число букв а: {} ".format(team_name.lower().count("а")))
